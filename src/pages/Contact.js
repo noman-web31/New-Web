b673c73de30f0e6d159f1f6f673562a6a0086ae4
Noman1331 <nomananoosh.0091@gmail.com>
2025-09-12 09:56:52 +0500

    Header and hero section add

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,8 +1,57 @@
 import React from 'react'
 
 const Contact = () => {
+
+  document.getElementById("myForm").addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+
+    if (name === "") {
+      alert("Please name field feel");
+      return;
+    }
+
+    if (email === "") {
+      alert("Please email field feel");
+      return;
+    }
+
+    if (password.length < 8) {
+      alert("password field feel");
+      return;
+    }
+    alert("form fill hogya");
+  })
+
   return (
-    <div>Contact</div>
+    <div>
+
+      <form action="" id='myForm'>
+        <h2>Logon Form</h2>
+        <div>
+          <label htmlFor="">Name</label>
+          <input type="text" name="name" id="" placeholder="Your name" />
+        </div>
+        <div>
+          <label htmlFor="">Email</label>
+          <input type="email" name="email" id="" placeholder="example@gmail.com" />
+        </div>
+        <div>
+          <label htmlFor="">Password</label>
+          <input type="password" name="password" id="" placeholder="********" />
+        </div>
+        <div>
+          <button type="submit">Login</button>
+        </div>
+
+
+      </form >
+
+    </div >
+
   )
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,9 @@
 import React from 'react'
+import { Link } from "react-router-dom";
+import { FaLinkedinIn } from "react-icons/fa";
+import { RiFacebookFill } from "react-icons/ri";
+import { FaTwitter } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
 
 const Home = () => {
   return (
@@ -8,8 +13,15 @@ const Home = () => {
         <div className="container">
           <h3>Hello It's me</h3>
           <h1>Noman</h1>
-          <h3>And I'm a Frontend Developer</h3>
+          <h3>And I'm a <span> Frontend Developer </span></h3>
           <p>Welcome to my portfolio! specialize in frontend development, focusing on creating intuitive and visually appealing user interfaces.</p>
+          <div className="social-icon">
+            <Link to="#"><FaLinkedinIn /></Link>
+            <Link to="#"><RiFacebookFill /></Link>
+            <Link to="#"><FaTwitter /></Link>
+            <Link to="#"><FaInstagram /></Link>
+          </div>
+          <Link to="#" className='HeroBtn'>Download CV</Link>
         </div>
       </section>
     </div>
